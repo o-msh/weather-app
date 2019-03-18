57c927d0e643a3bcb26b8ebfb2e9007d628c4cbc
Oleh <shtepa.oleh.m@gmail.com>
2019-03-19 01:26:09 +0200

    Add Temperature component

--- src/js/Components/App/App.js
+++ src/js/Components/App/App.js
@@ -1,5 +1,11 @@
+import Temperature from '../Temperature';
+
 export default class App {
     constructor(host) {
-        host.innerHTML = 'Weather';
+
+        host.innerHTML = '';
+        const temperature1 = document.createElement('div');
+        new Temperature(temperature1);
+        host.append(temperature1);
     }
 }
---
+++ src/js/Components/Temperature/Temperature.js
@@ -0,0 +1,5 @@
+export default class Temperature {
+    constructor(host) {
+        host.innerHTML = '7&deg;C';
+    }
+}
