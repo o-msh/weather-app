cc934ba591d30c4f62cf62e21a8b372a0a8e0b48
Oleh <shtepa.oleh.m@gmail.com>
2019-03-20 10:28:02 -0400

    Add Wind component

--- src/js/Components/App/App.js
+++ src/js/Components/App/App.js
@@ -1,15 +1,53 @@
-import Component from '../../framework/Component';
+import Component from '../../Framework/Component';
 import Temperature from '../Temperature';
+import Wind from '../Wind';
 
 export default class App extends Component {
     constructor(host) {
         super(host);
     }
+    
     render() {
         const t1 = document.createElement('div');
-        new Temperature(t1);
-        const t2 = document.createElement('div');
-        new Temperature(t2);
-        return ['Temperature range', t1, t2];
+        new Temperature(t1, { temperature: 25, unit: 'C' });
+        const w1 = document.createElement('div');
+        new Wind(w1, { speed: 100500, unit: 'mph' });
+        return [
+            'Temperature range',
+            t1,
+            w1,
+            {
+                tag: Wind,
+                props: {
+                    speed: 250,
+                    unit: 'mph',
+                },
+            },
+            {
+                tag: Temperature,
+                props: {
+                    temperature: 7,
+                    unit: 'C',
+                },
+            },
+            {
+                tag: Temperature,
+                props: {
+                    temperature: 18,
+                    unit: 'C',
+                },
+            },
+            {
+                tag: 'div',
+                content: 'Me div',
+                classList: ['nice'],
+                attributes: [
+                    {
+                        name: 'title',
+                        value: 'Me definetely nice div',
+                    },
+                ],
+            },
+        ];
     }
 }
---
+++ src/js/Framework/Component.js
@@ -0,0 +1,58 @@
+export default class Component {
+    constructor(host, props = {}) {
+        this.host = host;
+        this.props = props;
+        this._render();
+    }
+
+    _render() {
+        this.host.innerHTML = '';
+        const content = this.render();
+        if (typeof content === 'string') {
+            this.host.innerHTML = content;
+        } else {
+            content.map(this._vDomPrototypeElementToHtmlElement)
+                .forEach(htmlElement => {
+                    this.host.appendChild(htmlElement);
+                });
+        }
+    }
+
+    render() { }
+
+    _vDomPrototypeElementToHtmlElement(element) {
+        if (typeof element === 'string') {
+            const htmlElement = document.createElement('div');
+            htmlElement.innerHTML = element;
+            return htmlElement;
+        } else {
+            if (element.tag) {
+                if (typeof element.tag === 'function') {
+                    const container = document.createElement('div');
+                    new element.tag(container, element.props);
+                    return container;
+                } else {
+                    const container = document.createElement(element.tag);
+                    ['classList', 'attributes'].forEach(item => {
+                        if (element[item] && !Array.isArray(element[item])) {
+                            element[item] = [element[item]];
+                        }
+                    });
+                    if (element.content) {
+                        container.innerHTML = element.content;
+                    }
+                    if (element.classList) {
+                        container.classList.add(...element.classList);
+                    }
+                    if (element.attributes) {
+                        element.attributes.forEach(attributeSpec => {
+                            container.setAttribute(attributeSpec.name, attributeSpec.value);
+                        });
+                    }
+                    return container;
+                }
+            }
+            return element;
+        }
+    }
+}
